7cc5c40994b70cf3dd56fb07b7cdb570bc84d1d0
h-shige <h.shigematsu0510@gmail.com>
2023-06-15 12:45:41 +0900

    コメントの追加

--- src/main/java/com/example/ads/config/SecurityConfig.java
+++ src/main/java/com/example/ads/config/SecurityConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
- *
+ * セキュリティ設定ファイル
  */
 @RequiredArgsConstructor
 @Configuration
